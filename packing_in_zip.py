5363a7e6779d71c0b07a3bc02c301186ab67bf52
Anton Cherentsov <antche17@gmail.com>
2025-07-31 22:49:27 -0700

    changed path of archive.zip

--- packing_in_zip.py
+++ packing_in_zip.py
@@ -1,11 +1,15 @@
 import os
 import zipfile
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parent
+FILES_DIR = BASE_DIR / "FILES"
+ARCHIVE_PATH = BASE_DIR / "archive.zip"
 
 def create_archive():
-    with zipfile.ZipFile('/Users/antche/PycharmProjects/qa_guru/qa_guru_7_hw/archive.zip', 'w') as zf:
+    with zipfile.ZipFile(ARCHIVE_PATH, 'w') as zf:
         for file in ('example.csv', 'example.xlsx', 'example.pdf'):
-            add_file = os.path.join('/Users/antche/PycharmProjects/qa_guru/qa_guru_7_hw/FILES',file)
+            add_file = FILES_DIR / file
             zf.write(add_file, os.path.basename(add_file))
             print("Архив создан!")
 
